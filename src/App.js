9cd6f7bc2b0130b5a425e173a72af7bb21a6c081
Angelo Martínez <ammustang@gmail.com>
2021-04-17 01:28:39 -0500

    added loading icon

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ import Map from "./Map";
 import Info from "./Info";
 import Plots from "./Plots";
 import "./styles/App.css";
+import style from "./styles/App.module.css";
 
+//spiner import
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 // TODO comment everything...the thing in the provider is kind of confusing
+
+function LoadScreen() {
+  return (
+    <div className={style.loadScreen}>
+      <Loader type="Grid" color="White" height={80} width={80} />
+    </div>
+  );
+}
 
 function App() {
   const { loadingAll, errorAll } = useContext(DataContext);
 
-  if (loadingAll) return <h1>Loading...</h1>;
+  if (loadingAll) return <LoadScreen />;
   if (errorAll) return <p>{JSON.stringify(errorAll, null, 2)}</p>;
 
   return (
